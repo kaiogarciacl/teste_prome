a6c93a35839dfdca77a9acbf58ac2e5362bcdcff
kaiogarciacl <kaio.garcia@clcaldeiraria.com.br>
2024-06-18 07:30:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="PROMETHEUS",layout="wide", initial_sidebar_state=
 with open("style.css") as f:st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-with st.sidebar?
+with st.sidebar:
     
     log = st.text_input('LOG', label_visibility='collapsed', placeholder='Login', key='Log')
     senha = st.text_input('senha', label_visibility='collapsed', placeholder='Senha', key='senha', type='password')
