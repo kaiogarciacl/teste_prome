17eb596c0a22a408ccf8ec69443c133b5cfe95aa
kaiogarciacl <kaio.garcia@clcaldeiraria.com.br>
2024-06-18 08:36:30 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -7,18 +7,23 @@ import os
 
 
 def teste():
+    comando = st.text_area('comando')
 
-    # Everything is accessible via the st.secrets dict:
-    st.write("DB username:", st.secrets["db_username"])
-    st.write("DB password:", st.secrets["db_password"])
-    st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
-    
-    # And the root-level secrets are also accessible as environment variables:
-    st.write(
-        "Has environment variables been set:",
-        os.environ["db_username"] == st.secrets["db_username"],
-    )
+    def vai():
+        md.executar = comando
+        comandos.vai_porra()
 
 
-    
-    
+    st.button('vai', on_click=vai)
+
+    rows = f""" select * from [3_tabela_comandos] """
+    cursor = cn.conexao_sql.cursor()
+    cursor.execute(rows)
+    rows = cursor.fetchall()
+    for row in rows:
+        rows = f""" {row[0]} """
+        cursor = cn.conexao_sql.cursor()
+        cursor.execute(rows)
+        rows = cursor.fetchall()
+        for row in rows:
+            st.markdown(row[0])
