2e65b9a4214a1358972a62e26a2020a67bc85716
kaiogarciacl <kaio.garcia@clcaldeiraria.com.br>
2024-06-18 08:22:18 -0300

    Update conexao.py

--- conexao.py
+++ conexao.py
@@ -3,28 +3,49 @@ import models as md
 import time
 import secrets as st
 
-conexao_sql = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+
+
+
+
+
+def pesquisar_tabela(query):
+    with conexao_sql = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER="
         + st.secrets["server"]
         + ";DATABASE="
         + st.secrets["database"]
         + ";UID="
         + st.secrets["username"]
         + ";PWD="
-        + st.secrets["password"])
-
-
-
-
-def pesquisar_tabela(query):
-    with conexao_sql.cursor() as cur:
+        + st.secrets["password"]).cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
 
 
 def ins_upd_del():
-    conexao_sql.cursor()
-    conexao_sql.execute(f'{md.comando}')
-    conexao_sql.commit()
+    conexao_sql = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + st.secrets["server"]
+        + ";DATABASE="
+        + st.secrets["database"]
+        + ";UID="
+        + st.secrets["username"]
+        + ";PWD="
+        + st.secrets["password"]).cursor()
+    conexao_sql = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + st.secrets["server"]
+        + ";DATABASE="
+        + st.secrets["database"]
+        + ";UID="
+        + st.secrets["username"]
+        + ";PWD="
+        + st.secrets["password"]).execute(f'{md.comando}')
+    conexao_sql = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + st.secrets["server"]
+        + ";DATABASE="
+        + st.secrets["database"]
+        + ";UID="
+        + st.secrets["username"]
+        + ";PWD="
+        + st.secrets["password"]).commit()
 
 
 
