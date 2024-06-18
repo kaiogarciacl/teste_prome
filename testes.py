e9c79403498e910ef8fa8a506a0add9d3b854534
kaiogarciacl <kaio.garcia@clcaldeiraria.com.br>
2024-06-18 08:49:34 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -9,18 +9,5 @@ def teste():
     def vai():
         md.executar = comando
         comandos.vai_porra()
-
-
+        
     st.button('vai', on_click=vai)
-
-    rows = f""" select * from [3_tabela_comandos] """
-    cursor = cn.conexao_sql.cursor()
-    cursor.execute(rows)
-    rows = cursor.fetchall()
-    for row in rows:
-        rows = f""" {row[0]} """
-        cursor = cn.conexao_sql.cursor()
-        cursor.execute(rows)
-        rows = cursor.fetchall()
-        for row in rows:
-            st.markdown(row[0])
