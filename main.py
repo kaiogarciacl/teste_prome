dd34cf4f49ec374f2b926c7535c985dd30c51daf
kaiogarciacl <kaio.garcia@clcaldeiraria.com.br>
2024-06-18 09:28:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import hydralit_components as hc
 from streamlit_modal import Modal
 import testes
+import models as md
 
 
 st.set_page_config(page_title="PROMETHEUS",layout="wide", initial_sidebar_state="collapsed")
