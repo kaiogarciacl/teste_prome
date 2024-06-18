1b7f2da8a6c245ca034ec3f94f112c4258f8fb62
kaiogarciacl <kaio.garcia@clcaldeiraria.com.br>
2024-06-18 07:35:22 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,8 +1,28 @@
 import streamlit as st
-from streamlit_modal import Modal
-import hydralit_components as hc
+import comandos
+import conexao as cn
+import models as md
+
 
 
 def teste():
-    st.title('TESTE')
+    comando = st.text_area('comando')
 
+    def vai():
+        md.executar = comando
+        comandos.vai_porra()
+
+
+    st.button('vai', on_click=vai)
+
+    rows = f""" select * from [3_tabela_comandos] """
+    cursor = cn.conexao_sql.cursor()
+    cursor.execute(rows)
+    rows = cursor.fetchall()
+    for row in rows:
+        rows = f""" {row[0]} """
+        cursor = cn.conexao_sql.cursor()
+        cursor.execute(rows)
+        rows = cursor.fetchall()
+        for row in rows:
+            st.markdown(row[0])
