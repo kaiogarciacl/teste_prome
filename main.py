75a428ba2dd9361f8782bdd46939c49769294c5a
kaiogarciacl <kaio.garcia@clcaldeiraria.com.br>
2024-06-26 17:50:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import hydralit_components as hc
+
 from streamlit_modal import Modal
 import testes
 import models as md
@@ -16,27 +16,11 @@ with st.sidebar:
     log = st.text_input('LOG', label_visibility='collapsed', placeholder='Login', key='Log')
     senha = st.text_input('senha', label_visibility='collapsed', placeholder='Senha', key='senha', type='password')
 
-if log != '' and senha != '' :
-    menu_opcao = [
-    
-        {'icon': 'bi bi-bar-chart', 'label': 'Indicadores'},
-    
-        {'icon': 'bi bi-person-circle', 'label': 'Conta',
-         'submenu': [{'icon': 'bi bi-list-task', 'label': 'Ponto'},
-                     {'icon': 'bi bi-person-plus', 'label': 'Configuração da Conta'}]},
-    
-        {'icon': 'bi bi-person-circle', 'label': 'Teste'}]
+if log != '' and senha != '' : 
+    comando = st.text_area('comando')
 
-    
-    tema = {'txc_inactive': 'black', 'menu_background': '#eaeaea', 'txc_active': 'black', 'option_active': '#ffffff', 'gap': '0rem'}
-    opcao = hc.nav_bar(menu_definition=menu_opcao, home_name='Ambiente', override_theme=tema, sticky_mode='sticky', sticky_nav=False)
-
-    if opcao == 'Teste':
+    def vai():
+        md.executar = comando
+        comandos.vai_porra()
         
-        comando = st.text_area('comando')
-
-        def vai():
-            md.executar = comando
-            comandos.vai_porra()
-            
-        st.button('vai', on_click=vai)
+    st.button('vai', on_click=vai)
